95c6aa670f6c679711a86c6a8f889572201fe358
Skyner <sacwks@gmailcom>
2020-03-21 15:30:29 -0400

    Finalizado el video 379

--- inc/templates/footer.php
+++ inc/templates/footer.php
@@ -8,6 +8,7 @@
 
     if ($actual === 'crear-cuenta' || $actual === 'login') {
         echo '<script src="js/formulario.js"></script>';
+        //echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>';
     }
 
 ?>
--- js/formulario.js
+++ js/formulario.js
@@ -1,1 +1,25 @@
-console.log('funciona!!');
+eventListeners();
+
+function eventListeners(){
+    document.querySelector('#formulario').addEventListener('submit', validarRegistro);
+}
+
+function validarRegistro(e){
+    // preventDefault se utiliza para impedir que se envie un formulario, que es su accion predeterminada al pulsar el boton submit
+    e.preventDefault();
+    // console.log('aqui vamos');
+
+    //Para validar que los campos tengan algo escrito
+    var usuario = document.querySelector('#usuario').value,
+        password = document.querySelector('#password').value;
+        //console.log(usuario + "" + password);
+    if (usuario === '' || password === '') {
+        Swal.fire({
+            type: 'error',
+            title: 'Error',
+            text: '¡Ambos campos son obligatorios!'
+          })
+    }else{
+        
+    }
+}
