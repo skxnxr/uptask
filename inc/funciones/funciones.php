5234d686ff6aa92fa4150584da749d6539de52b2
Skyner <sacwks@gmailcom>
2020-03-21 14:29:20 -0400

    min 6.22

--- inc/funciones/funciones.php
+++ inc/funciones/funciones.php
@@ -2,6 +2,7 @@
 
 function obtenerPaginaActual(){
     $archivo = basename($_SERVER['PHP_SELF']);
-    echo $archivo;
+    $pagina = str_replace(".php", "", $archivo);
+    echo $pagina;
 }
 obtenerPaginaActual();
