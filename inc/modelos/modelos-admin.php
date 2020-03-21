50bca423b9bf1c4cf4868d13470f164ed19cfebc
Skyner <sacwks@gmailcom>
2020-03-21 17:55:39 -0400

    Finalizado el video 382

--- inc/modelos/modelos-admin.php
+++ inc/modelos/modelos-admin.php
@@ -13,10 +13,34 @@ if($accion === 'crear'){
     );
     $hash_password = password_hash($password, PASSWORD_BCRYPT, $opciones);
 
-    $respuesta = array(
-        'pass' => $hash_password
-    );
-    echo json_encode($respuesta);
+    //Importar la conexion
+    include '../funciones/conexion.php';
+    try {
+        //Realiza la consulta a la base de datos
+        $stmt = $conn->prepare("INSERT INTO usuarios (usuario, password) VALUES (?,?)");
+        $stmt->bind_param('ss', $usuario, $hash_password);
+        $stmt->execute();
+        if ($stmt->affected_rows) {
+            $respuesta = array(
+                'respuesta' => 'correcto',
+                'id_insertado' => $stmt->insert_id,
+                'tipo' => $accion
+            );
+        }
+        $stmt->close();
+        $conn->close();
+    } catch (Exception $e) {
+        //En caso de error, tomar la excepción
+        $respuesta = array(
+                'error' => $e->getMessage()
+            );
+    }
+
+        // $respuesta = array(
+        //     'pass' => $hash_password
+        // );
+     echo json_encode($respuesta);
+
 }
 
 if($accion === 'login'){
