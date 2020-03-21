83ce9826ee7215d6fa3329bf391850a5f236519c
Skyner <sacwks@gmailcom>
2020-03-21 16:09:26 -0400

    Finalizado el video 381

--- inc/modelos/modelos-admin.php
+++ inc/modelos/modelos-admin.php
@@ -1,3 +1,24 @@
 <?php
 
-die(json_encode($_POST));
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+$accion = $_POST['accion'];
+
+if($accion === 'crear'){
+    //Código para crear los administradores
+
+    //hashear passwords
+    $opciones = array(
+        'cost' => 12
+    );
+    $hash_password = password_hash($password, PASSWORD_BCRYPT, $opciones);
+
+    $respuesta = array(
+        'pass' => $hash_password
+    );
+    echo json_encode($respuesta);
+}
+
+if($accion === 'login'){
+    //Código loguear los administradores
+}
