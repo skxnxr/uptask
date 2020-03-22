4d28a4e33676c51b7968d8e7d4ceb54b4ee5a5b2
Skyner <sacwks@gmailcom>
2020-03-22 12:02:21 -0400

    Finalizado el video 390

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 eventListeners();
 
+//Lista de proyectos
+var listaProyectos = document.querySelector('ul#proyectos');
+
 function eventListeners() {
     // boton para crear proyecto
     document.querySelector('.crear-proyecto a').addEventListener('click', nuevoProyecto);
@@ -9,4 +12,28 @@ function eventListeners() {
 function nuevoProyecto(e) {
     e.preventDefault(); 
     console.log('Presionaste en nuevo proyecto');
+
+    //Crea un input para el nombre del nuevo proyecto
+    var nuevoProyecto = document.createElement('li');
+    nuevoProyecto.innerHTML = '<input type ="text" id="nuevo-proyecto">';
+    listaProyectos.appendChild(nuevoProyecto);
+
+    //Seleionar el ID con el nuevo proyecto
+    var inputNuevoProyecto = document.querySelector('#nuevo-proyecto');
+
+    //Al presionar enter crea el nuevo proyecto
+    inputNuevoProyecto.addEventListener('keypress', function(e){
+       var tecla = e.which || e.keyCode;
+       
+       if (tecla === 13) {
+        //    console.log('Presionaste Enter');
+            guadarProyectoDB(inputNuevoProyecto.value);
+            listaProyectos.removeChild(nuevoProyecto);
+       }
+    });
+
+}
+
+function guadarProyectoDB(nombreProyecto) {
+    console.log(nombreProyecto);
 }
